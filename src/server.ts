00131643dd826c205932615f488f8efb1aff2f07
magnanimox <tiagoeds96@gmail.com>
2022-10-18 11:11:32 -0300

    final project

--- src/server.ts
+++ src/server.ts
@@ -17,8 +17,9 @@ server.use(express.static(path.join(__dirname, "../public")));
 // Rotas
 server.use(mainRoutes);
 
+// Pagina não encontrada
 server.use((req, res) => {
-    res.send("página não encontrada!");
+    res.render("pages/404");
 });
 
 server.listen(process.env.PORT);
